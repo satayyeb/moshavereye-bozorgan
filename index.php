4dcafe27f539b6367263f9b2370d6efbf9279837
satayyeb <salitayyeb@gmail.com>
2021-12-17 13:22:50 +0330

    make it ok for php 8

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php
 
-//some function declarations for older PHP versions(under php 8)
-function str_starts_with ( $haystack, $needle ) {
-    return strpos( $haystack , $needle ) === 0;
-} 
-function str_ends_With($haystack, $needle) {
-    $length = strlen($needle);
-    return $length > 0 ? substr($haystack, -$length) === $needle : true;
-}
-
 //get the names from json file
 $json = json_decode(file_get_contents('people.json'), true);
 $en_names_array = array_keys($json);
